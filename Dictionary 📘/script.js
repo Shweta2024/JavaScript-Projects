fc7a462e1586d24ca5f3897888cc4f1bb65daa3c
Rahil Saini <118007944+madeupgalaxy@users.noreply.github.com>
2022-12-21 17:03:24 +0530

    Changed file names and Added comments

--- Dictionary 📘/script.js
+++ Dictionary 📘/script.js
@@ -4,8 +4,10 @@ searchInput = stylish.querySelector("input"),
 volume = stylish.querySelector(".word i"),
 infoText = stylish.querySelector(".info-text"),
 removeIcon = stylish.querySelector(".search span");
-let audio;
 
+
+
+// Declaring the function for search 
 function data(result, word){
     if(result.title){
         infoText.innerHTML = `Can't find the meaning of <span>"${word}"</span>. Check it's spelling or try another word !`;
@@ -15,13 +17,12 @@ function data(result, word){
         phontetics = `${result[0].meanings[0].partOfSpeech}  /${result[0].phonetics[0].text}/`;
         document.querySelector(".word p").innerText = result[0].word;
         document.querySelector(".word span").innerText = phontetics;
-        document.querySelector(".meaning span").innerText = definitions.definition;
-        document.querySelector(".example span").innerText = definitions.example;
-        audio = new Audio(result[0].phonetics[0].audio);
+        document.querySelector(".meaning span").innerText = definitions.definition;      
 
     }
 }
 
+// function for fetching the word using Dictionary APT
 function search(word){
     fetchApi(word);
     searchInput.value = word;
@@ -32,12 +33,13 @@ function fetchApi(word){
     stylish.classList.remove("active");
     infoText.style.color = "#000";
     infoText.innerHTML = `Searching the meaning of <span>"${word}"</span>`;
-    let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;     // Dictionary API 
     fetch(url).then(response => response.json()).then(result => data(result, word)).catch(() =>{
         infoText.innerHTML = `Can't find the meaning of <span>"${word}"</span>. Please, try to search for another word.`;
     });
 }
 
+// Storing the word fetched by fetchAPI 
 searchInput.addEventListener("keyup", e =>{
     let word = e.target.value.replace(/\s+/g, ' ');
     if(e.key == "Enter" && word){
@@ -45,14 +47,7 @@ searchInput.addEventListener("keyup", e =>{
     }
 });
 
-volume.addEventListener("click", ()=>{
-    volume.style.color = "#4D59FB";
-    audio.play();
-    setTimeout(() =>{
-        volume.style.color = "#999";
-    }, 800);
-});
-
+//To be Displayed in Search box 
 removeIcon.addEventListener("click", ()=>{
     searchInput.value = "";
     searchInput.focus();
