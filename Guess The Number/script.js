67fd021c40c52451aa2f7bf3490f069fbd39c37d
pinkuagrawal28 <you@example.com>
2022-12-26 12:48:21 +0530

    added the comments in js file.

--- Guess The Number/script.js
+++ Guess The Number/script.js
@@ -5,32 +5,46 @@ var msg4 = document.getElementById("mess_level");
 var answer = Math.floor(Math.random() * 100) + 1;
 var no_of_guesses = 0;
 var guesses_num = [];
-
+// Function execution on button submission
 function play() {
   var level_guess = document.getElementById("lvl_guess").value;
   var user_guess = document.getElementById("guess").value;
+  //check for valid level
   if (level_guess < 1 || level_guess > 10) {
     alert("Please enter a level between 1 to 10");
-  } else {
+  } 
+  //if level is valid
+  else {
+    // check for valid guess
     if (user_guess < 1 || user_guess > 100) {
       alert("Please Enter a number Between 1 to 100");
-    } else {
+    } 
+    // if guess is valid
+    else {
       guesses_num.push(user_guess);
       no_of_guesses += 1;
+      // Condition for lossing the Game
       if (no_of_guesses == level_guess) {
         msg1.textContent = "You Lost The Game!!";
         msg2.textContent =
           "the Number was " + answer + " You were out of guesses ";
-      } else {
+      }
+      // Continue the game
+      else {
+        // when user guessed less than answer
         if (user_guess < answer) {
           msg1.textContent = "Your Guess is Too low";
           msg2.textContent = "No. Of Guesses : " + no_of_guesses;
           msg3.textContent = "Guessed Number Are: " + guesses_num;
-        } else if (user_guess > answer) {
+        } 
+        // when user guessed is greater than answer
+        else if (user_guess > answer) {
           msg1.textContent = "Your Guess is Too High";
           msg2.textContent = "No. Of Guesses : " + no_of_guesses;
           msg3.textContent = "Guessed Number Are: " + guesses_num;
-        } else if (user_guess == answer) {
+        }
+        // Condition for winning the game.
+         else if (user_guess == answer) {
           msg1.textContent = "Yahhhh You won It!!";
           msg2.textContent =
             "the Number was " +
